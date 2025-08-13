be85b5528ad4b08eebcf673dbad58a41dc09bf1c
ZAIDETH <z.rioscruzado@gmail.com>
2025-08-13 04:13:28 -0500

    solo se carga ollas comunes

--- js/qgis2web_expressions.js
+++ js/qgis2web_expressions.js
@@ -754,30 +754,3 @@ function fnc__specialcol_(values, context) {
 function fnc_project_color(values, context) {
     return false;
 };
-
-
-
-function exp_UsuariosdeOllasComunesgeorreferenciadas_2rule0_eval_expression(context) {
-    // 
-
-    var feature = context.feature;
-    
-    if (feature.properties) {
-        return null;
-    } else {
-        return null;
-    }
-}
-
-
-function exp_UsuariosdeOllasComunesgeorreferenciadas_2rule0_eval_expression(context) {
-    // 
-
-    var feature = context.feature;
-    
-    if (feature.properties) {
-        return null;
-    } else {
-        return null;
-    }
-}
